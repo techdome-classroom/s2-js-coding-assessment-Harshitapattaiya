691f8a243538e1191e82d19a03a6f9468c7b3933
Harshitapattaiya-03 <harshitapattaiya@gmail.com>
2024-09-30 14:25:21 +0530

    passed

--- program2.js
+++ program2.js
@@ -3,8 +3,30 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    let r = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let res = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let curr = r[s[i]];
+        let n= r[s[i + 1]];
+
+        if (n && curr < n) {
+            res -= curr;
+        } else {
+            res += curr;
+        }
+    }
+
+    return res;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
