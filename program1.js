dfb196908e1e8f5d18e224acd9ab5f3007327e85
Harshitapattaiya-03 <harshitapattaiya@gmail.com>
2024-09-30 14:32:39 +0530

    passes

--- program1.js
+++ program1.js
@@ -3,9 +3,28 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack = [];
+    let m = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+
+        if (char in m) {
+            let t= stack.length ? stack.pop() : '#';
+
+            if (m[char] !== t) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
